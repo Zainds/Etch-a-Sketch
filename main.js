3d531c4a4187af6d76832abd0afa7784c2a87430
Zainds <beat1097@mail.ru>
2025-03-25 21:56:31 +0700

    Calculate box size depending on grid element count

--- main.js
+++ main.js
@@ -1,27 +1,43 @@
 const container = document.querySelector('.container')
 const div = document.createElement('div')
+const rowDiv = document.createElement('div')
+const resetBtn = document.querySelector('button')
+const gridSize = 800
 
-div.style.border = "2px solid black"
-div.style.width = "30px"
-div.style.height = "30px"
+div.style.border = "1px solid black"
 div.className = "box"
 
-const rowDiv = document.createElement('div')
 rowDiv.style.display = 'flex'
 
-
-for(let i = 0; i < 16; i ++){
-    const thisRow = rowDiv.cloneNode()
-    container.appendChild(thisRow)
-    for(let j = 0; j < 16; j ++){
-        const newDiv = div.cloneNode()
-        thisRow.appendChild(newDiv)
-    }
-    
-}
+drawGrid(17)
 container.addEventListener("mouseover", (event) => {
     const target = event.target
     if(target.className == 'box'){
         target.style.background = 'gray'
     }
 })
+resetBtn.addEventListener('click', () => {
+    const n = prompt('Enter n squares in new n x n grid(0..100):')
+    if (n > 100 || n < 0 || !(typeof +n ==='number')){
+        alert('Enter valid amount of squares.')
+    }else{
+        while(container.firstChild){
+            container.removeChild(container.firstChild)
+        }
+        drawGrid(n)
+    }
+})
+function drawGrid(n){
+    const boxSize = gridSize/n
+    for(let i = 0; i < n; i ++){
+        const thisRow = rowDiv.cloneNode()
+        container.appendChild(thisRow)
+        for(let j = 0; j < n; j ++){
+            const newDiv = div.cloneNode()
+            newDiv.style.width = boxSize + 'px'
+            newDiv.style.height = boxSize + 'px'
+            thisRow.appendChild(newDiv)
+        }
+        
+    }
+}
